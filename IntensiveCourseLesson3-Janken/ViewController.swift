6cec91a31126e85656bffea31f9eeec48cbe87c0
sagaraEito <gjmptw.aab@gmail.com>
2021-04-04 00:12:21 +0900

    first commit

---
+++ IntensiveCourseLesson3-Janken/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  IntensiveCourseLesson3-Janken
+//
+//  Created by 相良 詠斗 on 2021/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var answerImageView: UIImageView!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    //じゃんけん（数字）
+    var answerNumber = 0
+    
+    @IBAction func shuffleAction(_ sender: Any) {
+        
+//        //0,1,2の数値をランダムに算出（乱数）
+//        answerNumber = Int.random(in: 0..<3)
+        
+        //新しいじゃんけんの結果を一時的に格納する変数を設ける
+        var newAnswerNumber = 0
+        
+        //ランダムに結果を出すが、前回の結果と異なる場合のみ採用
+        //repeatは繰り返しを意味する
+        repeat {
+            
+            //0,1,2の数値をランダムに算出
+            newAnswerNumber = Int.random(in: 0..<3)
+            
+            //前回と同じ結果のときは、再度、ランダムに数値を出す
+            //異なる結果のときは、repeatを抜ける
+        } while answerNumber == newAnswerNumber
+        
+        //新しいじゃんけんの結果を格納
+        answerNumber = newAnswerNumber
+        
+        
+        if answerNumber == 0 {
+            
+        //グー
+        answerLabel.text = "グー"
+        answerImageView.image = UIImage(named: "gu")
+            
+        } else if answerNumber == 1 {
+            
+            //チョキ
+            answerLabel.text = "チョキ"
+            answerImageView.image = UIImage(named: "choki")
+            
+        } else if answerNumber == 2 {
+            
+            //パー
+            answerLabel.text = "パー"
+            answerImageView.image = UIImage(named: "pa")
+            
+        }
+        
+//        //次のジャンケンへ
+//        answerNumber = answerNumber + 1
+            
+    }
+    
+}
+
